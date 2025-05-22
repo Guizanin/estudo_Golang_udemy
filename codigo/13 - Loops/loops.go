d09fd66522b672c030d77914c6c4f165310a7ee8
Guilherme Zanin <guilhermecasazanin@gmail.com>
2025-05-22 17:09:48 -0300

    fazendo loops

--- codigo/13 - Loops/loops.go
+++ codigo/13 - Loops/loops.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main(){
 	fmt.Println("Loops: ")
+
+	i := 0
+
+	//FORMA 1
+	for i < 10 {
+		fmt.Println("Incrementando i: ", i)
+		i++
+		time.Sleep(time.Second)
+	}
+
+	//FORMA 2
+	for j:= 0; j < 10; j++ {
+		fmt.Println("incremento forma 2 for: ", j)
+	}
+
+	//FORMA 3
+	//iterando em arrays
+	arrayNomes := [3]string{"jão", "Davi", "Golias"}
+	for indice, nome := range arrayNomes{
+		fmt.Println("indice: ", indice, "; nome: ", nome)
+	}
+
 }
